4a6dabbbd9ce958c1bdee28a37471b294abeac89
GATM <german.gt285@gmail.com>
2019-05-21 19:20:18 -0400

    holaaaa

---
+++ app/src/main/java/com/univalle/myapplication/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.univalle.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button atras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        atras = (Button)findViewById(R.id.atras);
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/univalle/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.univalle.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCelsiusFahrenheit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCelsiusFahrenheit = (Button)findViewById(R.id.btnCelsiusFahrenheit);
+        btnCelsiusFahrenheit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+    }
+}
